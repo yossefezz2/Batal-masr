d1325853a59266fac4489326a8f49b8ff7b00c63
Abdoreda2354 <abreda1678@gmail.com>
2024-02-15 01:42:34 +0200

    login

--- FrontEnd/src/app/core/services/auth.service.ts
+++ FrontEnd/src/app/core/services/auth.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+baseURL:string='http://localhost:3000/'
+  constructor(private _HttpClient:HttpClient) { }
+  login(userData:object):Observable<any>{
+    return this._HttpClient.post(`http://localhost:3000/login`,userData)
+  }
 }
