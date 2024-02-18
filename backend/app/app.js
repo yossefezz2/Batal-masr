3ae46058677340514ef19c523e92487839b9f390
yossefezz2 <ezzahmed597@gmail.com>
2024-02-19 01:50:34 +0200

    solve error in routes and add new function

--- backend/app/app.js
+++ backend/app/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser')
 const sharedRoutes = require('../routes/sharedRoutes')
 const mangeAgentsOfMinistryRoutes = require('../routes/admin/mangeAgentsOfMinistryRoutes')
 const mangeRepresentorOfAssociation = require('../routes/admin/mangeRepresentorOfAssociationRouts')
+const mangeAssociationRoutes = require('../routes/admin/mangeAssociationRouts')
 // const nanoid = require('nanoid');
 // const nId1 = nanoid(10);
 // console.log(nId1);
@@ -23,6 +24,7 @@ app.use(express.json())
 app.use("/",sharedRoutes)
 app.use("/admin/agent",mangeAgentsOfMinistryRoutes)
 app.use("/admin/representor",mangeRepresentorOfAssociation)
+app.use("/admin/association",mangeAssociationRoutes)
 app.all('*',(req,res)=>{
     res.send({
         massage:"not found"
