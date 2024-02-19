895291c9031a95b3975452700f602b0315bfcd7c
yossefezz2 <ezzahmed597@gmail.com>
2024-02-19 19:08:55 +0200

    logout

--- backend/app/controlar/shared.js
+++ backend/app/controlar/shared.js
@@ -1,13 +1,13 @@
-const sharedService =require("../../services/shared.js");
+const sharedService = require("../../services/shared.js");
 const helpers = require('../helper.js');
 const bcrypt = require("bcrypt");
-class shared{
+class shared {
     static async login(req, res) {
         try {
-            const findEmail =await sharedService.getEmail(req.body.email)
+            const findEmail = await sharedService.getEmail(req.body.email)
             if (findEmail[0]) {
                 if (await bcrypt.compare(req.body.password, findEmail[0].password)) {
-                    await sharedService.genToken(req,findEmail[0].id)
+                    await sharedService.genToken(req, findEmail[0].id)
                     findEmail[0].token = req.token
                     helpers.resGenerator(res, 200, true, findEmail[0], "success")
                 } else {
@@ -16,13 +16,31 @@ class shared{
             } else {
                 throw new Error("Invalid email")
             }
-            
+
 
         } catch (error) {
             console.log(error);
             helpers.resGenerator(res, 500, false, error.message, "can't login")
         }
     }
+    static async singleLogout(req, res) {
+        try {
+            await sharedService.singleLogout(req.token)
+            helpers.resGenerator(res, 200, true,{}, "success")
+        } catch (error) {
+            console.log(error);
+            helpers.resGenerator(res, 500, false, error.message, "can't log out")
+        }
+    }
+    static async logoutForAll(req, res) {
+        try {
+            await sharedService.logoutForAll(req.user.id)
+            helpers.resGenerator(res, 200, true,{}, "success")
+        } catch (error) {
+            console.log(error);
+            helpers.resGenerator(res, 500, false, error.message, "can't log out")
+        }
+    }
 
 }
-module.exports =shared;
+module.exports = shared;
--- backend/routes/sharedRoutes.js
+++ backend/routes/sharedRoutes.js
@@ -1,6 +1,8 @@
 var express = require('express');
-// const {userMiddleware,isAdmin} = require("../../app/middelware/auth.middelware")
+const {userMiddleware} = require("../app/middelware/auth.middelware.js")
 const shared = require('../app/controlar/shared.js')
 const router =express.Router()
 router.post("/login", shared.login)
+router.delete("/singleLogout",userMiddleware, shared.singleLogout)
+router.delete("/allLogout",userMiddleware, shared.logoutForAll)
 module.exports = router
