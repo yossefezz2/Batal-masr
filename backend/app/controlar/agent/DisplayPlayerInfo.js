3613e34e5eb533dbb250171e6d9d328d7a11c4bd
yossefezz2 <ezzahmed597@gmail.com>
2024-05-05 16:12:12 +0300

    solve some error

--- backend/app/controlar/agent/DisplayPlayerInfo.js
+++ backend/app/controlar/agent/DisplayPlayerInfo.js
@@ -1,6 +1,23 @@
 const helpers = require('../../helper.js');
 const DisplayPlayerInfoService = require('../../../services/agent/DisplayPlayerInfoService.js');
 class displayPlayerInfo{
+    static async getAllPlayersInSingleInAllProject(req,res){
+        try {
+            const data = await DisplayPlayerInfoService.getallPlayersInAllProject();
+            if (data.length <= 0) {
+                throw new Error("not player found");
+            }
+            helpers.resGenerator(res, 200, true, data, "all players")
+        }
+        catch (error) {
+            if (error.message == "not player found") {
+                helpers.resGenerator(res, 404, false, error.message, "not players found")
+            } else {
+                helpers.resGenerator(res, 500, false, error.message, "can't get all players")
+            }
+
+        }
+    }
     static async getAllPlayersInSingleAssociation(req,res){
         try {
             const data = await DisplayPlayerInfoService.getallPlayers(req.params.id);
