2e4dc30688d05f28fd67c5853c5d3090501777a1
yossefezz2 <ezzahmed597@gmail.com>
2024-06-08 05:04:09 +0300

    last commit insha Allah

--- backend/app/controlar/agent/useModel.js
+++ backend/app/controlar/agent/useModel.js
@@ -16,36 +16,49 @@ class useModel {
             let dataToSend = {
                 "features": []
             }
-            let singlePlayerInfo 
+            let allPlayersMadalforever = []
+            let allPlayersMadalLast5Years = []
+                
+            let singlePlayerInfo
             playersIds.forEach(element => {
                 console.log(element);
-                let allMedalsNumber=0
-                let NumberOfGoldMedal=0
+                let allMedalsNumber = 0
+                let NumberOfGoldMedal = 0
+                let object1 = []
                 lastFiveYearsPlayerInfo.forEach(item => {
-                    if(element==item.playerId){
-                        if (item.isWin=="yes") {
+                    if (element == item.playerId) {
+                        if (item.isWin == "yes") {
                             allMedalsNumber++
-                            if (item.typeOfMedal=='gold') {
+                            if (item.typeOfMedal == 'gold') {
                                 NumberOfGoldMedal++
                             }
                         }
+                        object1.push(item)
                     }
-
                 });
-                singlePlayerInfo={
-                    "id":element,
+                allPlayersMadalLast5Years.push(object1)
+                singlePlayerInfo = {
+                    "id": element,
                     "values": [NumberOfGoldMedal, allMedalsNumber]
                 }
                 dataToSend.features.push(singlePlayerInfo)
             });
+            playersIds.forEach(element => {
+                let object2 = []
+                allPlayersInfo.forEach(item => {
+                    if (element == item.playerId) {
+                        object2.push(item)
+                    }
+                });
+                allPlayersMadalforever.push(object2)
+            });
 
-            console.log(dataToSend.features);
 
             const response = await axios.post('http://localhost:4000/predict', dataToSend);
-            
+
             // 18>>>1.8 21>>>1.6 24>>>1.40 27>>>1.25 30 >>> 1.15 
 
-            helpers.resGenerator(res, 200, true, {perdiction:response.data,lastFiveYearsPlayerInfo,allPlayersInfo,players}, "all models"); // Use response.data to get the actual data
+            helpers.resGenerator(res, 200, true, { perdiction: response.data, allPlayersMadalforever, players, allPlayersMadalLast5Years }, "all models"); // Use response.data to get the actual data
             // helpers.resGenerator(res, 200, true, { lastFiveYearsPlayerInfo, allPlayersInfo, players }, "all models"); // Use response.data to get the actual data
         } catch (error) {
             // Extract relevant error information
